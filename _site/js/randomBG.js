888d765de6f93a37d5ac96e27a0faec8fedf1717
Jonas McGowan-Martin <jmcgowanmartin@wpi.edu>
2018-02-13 12:10:50 -0500

    background image stuff tweaks

--- _site/js/randomBG.js
+++ _site/js/randomBG.js
@@ -1,18 +1,21 @@
 function randombg(){
-	var rand = Math.floor(Math.random() * 4) + 0;
-	var imgs = ["<img src='/img/bgs/building.jpg' id='background'>",
-				"<img src='/img/bgs/dog.png' id='background'>",
-				"<img src='/img/bgs/city.jpg' id='background'>",
-				"<img src='/img/bgs/god.jpg' id='background'>"];
-	var imgSrcs = ["<a href='https://chromfell.artstation.com'>Background source, refresh for more cool art!</a>",
-				   "Original artwork. Refresh for more cool art!",
-				   "<a href='https://adammidd.artstation.com'>Background source, refresh for more cool art!</a>",
-				   "<a href='https://chingyeh.artstation.com'>Background source, refresh for more cool art!</a>"];
-	var titleStyles = ["5px 5px red",
-					   "5px 5px white",
-					   "5px 5px #10ff00",
-					   "5px 5px red"];
-	document.getElementById("random").innerHTML = imgs[rand];
-	document.getElementById("bartitle").style.textShadow = titleStyles[rand];
-	document.getElementById("imgsources").innerHTML = imgSrcs[rand];
+	//format: [image, source link, title style]
+	var imgs = [["<img src='/img/bgs/building.jpg' id='background'>",
+				 "<a style='color:black;' href='https://chromfell.artstation.com'>Background source, refresh for more cool art!</a>",
+				 "5px 5px red"],
+				["<img src='/img/bgs/dog.png' id='background'>",
+				 "Original artwork. Refresh for more cool art!",
+				 "5px 5px white"],
+				["<img src='/img/bgs/city.jpg' id='background'>",
+				 "<a style='color:black;' href='https://adammidd.artstation.com'>Background source, refresh for more cool art!</a>",
+				 "5px 5px #10ff00"],
+				["<img src='/img/bgs/god.jpg' id='background'>",
+				 "<a style='color:gray;' href='https://chingyeh.artstation.com'>Background source, refresh for more cool art!</a>",
+				 "5px 5px red"]
+				];
+
+	var rand = Math.floor(Math.random() * imgs.length) + 0;
+	document.getElementById("random").innerHTML = imgs[rand][0];
+	document.getElementById("imgsources").innerHTML = imgs[rand][1];
+	document.getElementById("bartitle").style.textShadow = imgs[rand][2];
 }
